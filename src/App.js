7ff03be362162b0447fe982a7e196095de9effe5
Harshitha-15 <66387132+Harshitha-15@users.noreply.github.com>
2023-01-18 12:23:04 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
+import {useEffect} from 'react';
 import './App.css';
 
 function App() {
+  
+
+  function handleCallbackResponse(response){
+    console.log("Encoded JWT ID token: " + response.credential);
+
+  }
+  useEffect(() => {
+    /*global google */
+    google.accounts.id.initialize({
+      client_id: "987640259517-1stdc8orv68q5j85t4krvgs6port9327.apps.googleusercontent.com",
+      callback: handleCallbackResponse
+
+      });
+
+      google.accounts.id.renderButton(
+        document.getElementById("signInDiv"),
+        { theme:"outline", size: "large"}
+      );
+
+
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="signInDiv"></div>
+      
     </div>
   );
 }
