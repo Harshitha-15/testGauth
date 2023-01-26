606add6e1362c611588c13ae6a5b392d60a867ae
Harshitha-15 <66387132+Harshitha-15@users.noreply.github.com>
2023-01-26 20:34:52 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,77 @@
 import logo from './logo.svg';
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
+import jwt_decode from "jwt-decode";
 import './App.css';
 
 function App() {
+  const [user, setUser] = useState({});
   
 
   function handleCallbackResponse(response){
+    console.log("Encoded refreshed token: " + response);
     console.log("Encoded JWT ID token: " + response.credential);
+    var userObject = jwt_decode(response.credential);
+    console.log(userObject);
+    setUser(userObject);
+    document.getElementById("signInDiv").hidden = true;
 
   }
-  useEffect(() => {
-    /*global google */
-    google.accounts.id.initialize({
-      client_id: "987640259517-1stdc8orv68q5j85t4krvgs6port9327.apps.googleusercontent.com",
+
+  function handlesignOut(event){
+    setUser({});
+    document.getElementById("signInDiv").hidden = false;
+  }
+
+
+  // useEffect(() => {
+  //   /*global google */
+  //   google.accounts.id.initialize({
+  //     client_id: "184143611841-t5th1u1ts9abhoo88p0npqb5tu6r7lhv.apps.googleusercontent.com",
+  //     callback: handleCallbackResponse
+
+  //     });
+
+  //     google.accounts.id.renderButton(
+  //       document.getElementById("signInDiv"),
+  //       { type: "icon", size: "large"}
+  //     );
+
+  //     google.accounts.id.prompt();
+
+ 
+  // }, []);
+
+  window.onload = () => {
+    window.google.accounts.id.initialize({
+      client_id: "184143611841-t5th1u1ts9abhoo88p0npqb5tu6r7lhv.apps.googleusercontent.com",
       callback: handleCallbackResponse
 
       });
 
-      google.accounts.id.renderButton(
+      window.google.accounts.id.renderButton(
         document.getElementById("signInDiv"),
-        { theme:"outline", size: "large"}
+        { type: "icon", size: "large"}
       );
 
+      window.google.accounts.id.prompt();
+  }
 
-  }, []);
+  
 
   return (
     <div className="App">
       <div id="signInDiv"></div>
+      { Object.keys(user).length !=0 && 
+      <button onClick={(e) => handlesignOut(e)}>Sign Out</button>
+
+      }
+      
+      { user &&
+      <div>
+        <img src = {user.picture}></img>
+        <h3> {user.name} </h3>
+        </div>
+      }
       
     </div>
   );
